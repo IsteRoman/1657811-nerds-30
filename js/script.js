fe2004cce523f40dfff0955db38910f58e15455e
IsteRoman <demonatr@mail.ru>
2021-02-12 20:32:09 +0500

    рефакторинг js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-const sliders = document.querySelector('.sliders');
+const sliders_block = document.querySelector('.sliders-block');
+const sliders = document.querySelectorAll('.slider');
+const slide_buttons = document.querySelectorAll('.slide-btn');
 const first_slide = document.querySelector('.first-slide');
 const first = document.querySelector('.first');
 const second_slide = document.querySelector('.second-slide');
@@ -9,31 +11,33 @@ const open_form = document.querySelector('.button-open-form');
 const modal_form = document.querySelector('.modal');
 const close_form = document.querySelector('.close');
 
-if (sliders) {
+if (sliders_block) {
+  const sliders_clear = function() {
+    for (let i = 0; i<sliders.length; i++) {
+      sliders[i].classList.add('element-hidden');
+    }
+  }
+  const button_clear = function() {
+    for (let i = 0; i<slide_buttons.length; i++) {
+      slide_buttons[i].classList.remove('slide-btn-active');
+    }
+  }
   first.addEventListener('click', function() {
+    sliders_clear();
+    button_clear();
     first_slide.classList.remove('element-hidden');
-    second_slide.classList.add('element-hidden');
-    third_slide.classList.add('element-hidden');
     first.classList.add('slide-btn-active');
-    second.classList.remove('slide-btn-active');
-    third.classList.remove('slide-btn-active');
   });
-
   second.addEventListener('click', function() {
-    first_slide.classList.add('element-hidden');
+    sliders_clear();
+    button_clear();
     second_slide.classList.remove('element-hidden');
-    third_slide.classList.add('element-hidden');
-    first.classList.remove('slide-btn-active');
     second.classList.add('slide-btn-active');
-    third.classList.remove('slide-btn-active');
   });
-
   third.addEventListener('click', function() {
-    first_slide.classList.add('element-hidden');
-    second_slide.classList.add('element-hidden');
+    sliders_clear();
+    button_clear();
     third_slide.classList.remove('element-hidden');
-    first.classList.remove('slide-btn-active');
-    second.classList.remove('slide-btn-active');
     third.classList.add('slide-btn-active');
   });
 }
